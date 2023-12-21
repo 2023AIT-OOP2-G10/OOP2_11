974ca4b474cb11215c307f7e0f0e6370c96f020f
ucbssnj <k22010kk@aitech.ac.jp>
2023-12-21 16:10:03 +0900

    Update facedetect.py

--- facedetect.py
+++ facedetect.py
@@ -4,6 +4,7 @@ import os
 # Haar Cascadeファイルのパス
 CASCADE_PATH = "OOP2_11/haarcascade_frontalface/haarcascade_frontalface.xml"
 
+
 # 顔検出関数
 def detect_faces(image_path, output_path):
     face_cascade = cv2.CascadeClassifier(CASCADE_PATH)
@@ -13,8 +14,11 @@ def detect_faces(image_path, output_path):
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     cv2.imwrite(output_path, image)
 
 
+def process_image(file_path, processed_folder):
+    processed_path = os.path.join(processed_folder, os.path.basename(file_path))
+    detect_faces(file_path, processed_path)
