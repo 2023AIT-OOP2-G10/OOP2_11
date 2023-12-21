c1c3007d0769bd6fcc8360db940901e89a05f8bb
tou8 <153729655+tou8@users.noreply.github.com>
2023-12-21 15:48:48 +0900

    canny&grayscale_change

--- grayscale.py
+++ grayscale.py
@@ -1,17 +1,8 @@
 import cv2
-import numpy as np
+#import numpy as np
 
 im = cv2.imread('data/src/lena.jpg')
-print(im.shape)
-# (225, 400, 3)
 
-print(im.dtype)
-# uint8
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-print(im_gray.shape)
-# (225, 400)
-
-print(im_gray.dtype)
-# uint8
 
 cv2.imwrite('data/dst/opencv_gray_cvtcolr.jpg', im_gray)
